faf621a116cc76a031324597183aa952f33cf9f1
Lauren <lcc4935@rit.edu>
2021-09-30 20:03:23 -0400

    Comments on what to do

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-let nameArr = ['Eric L. Adams', 'Kathryn Garcia', 'Scott M. Stringer', 'Dianne Morales', 'Shaun Donovan', 'Art Chang', 'Isaac Wright Jr.','Maya D. Wiley', 'Andrew Young', 'Raymond J. Mcguire', 'Paperboy Love Prince', 'Aaron S. Foldenaur', 'Joycelyn Taylor', 'Write in Candidate'];
+//Everything we need to do:
+
+//states for which canidate is being picked (1st choice, 2nd choice, etc)
+//state changes based on confirmation pop-up
+//edit button (which is on the slots) brings to that specific case
+//html buttons/ slot fills are based on the array of canidates
+
+//submission confirmation: ballet and each canidate choice
+
+//keyboard for write-in? : keyboard button that shows keyboard image
+
+//css
+
+
+
+let nameArr = ['Eric L. Adams', 'Kathryn Garcia', 'Scott M. Stringer', 'Dianne Morales', 'Shaun Donovan', 
+'Art Chang', 'Isaac Wright Jr.','Maya D. Wiley', 'Andrew Young', 'Raymond J. Mcguire', 
+'Paperboy Love Prince', 'Aaron S. Foldenaur', 'Joycelyn Taylor', 'Write in Candidate'];
