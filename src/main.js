f3db1b1909bb104c93e67f40e6f88e71c24ba68b
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2021-10-03 13:31:27 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 //Everything we need to do:
-
+let orderedCandidate = []
 //states for which candidate is being picked (1st choice, 2nd choice, etc)
+function candidateSelection(candidate){
+    orderedCandidate.push(candidate)
+    console.log(orderedCandidate);
+
+    return orderedCandidate;
+}
+
 //state changes based on confirmation pop-up
 //edit button (which is on the slots) brings to that specific case
 //html buttons/ slot fills are based on the array of candidates
 
 //submission confirmation: ballet and each candidate choice
+
+function ballotSubmission(){
+
+    console.log(orderedCandidate);
+    return
+}
+
 
 //keyboard for write-in? : keyboard button that shows keyboard image
 
@@ -13,6 +27,6 @@
 
 
 
-let nameArr = ['Eric L. Adams', 'Kathryn Garcia', 'Scott M. Stringer', 'Dianne Morales', 'Shaun Donovan', 
-'Art Chang', 'Isaac Wright Jr.','Maya D. Wiley', 'Andrew Young', 'Raymond J. Mcguire', 
-'Paperboy Love Prince', 'Aaron S. Foldenaur', 'Joycelyn Taylor', 'Write in Candidate'];
+// let nameArr = ['Eric L. Adams', 'Kathryn Garcia', 'Scott M. Stringer', 'Dianne Morales', 'Shaun Donovan', 
+// 'Art Chang', 'Isaac Wright Jr.','Maya D. Wiley', 'Andrew Young', 'Raymond J. Mcguire', 
+// 'Paperboy Love Prince', 'Aaron S. Foldenaur', 'Joycelyn Taylor', 'Write in Candidate'];
