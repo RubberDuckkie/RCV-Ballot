217b2752d1f0a8184923285d50f23f414998bd17
Lauren <lcc4935@rit.edu>
2021-10-10 20:19:49 -0400

    Added state change

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ let finalCandidates = [];
 let choiceNum = 1;
 
 //states for which candidate is being picked (1st choice, 2nd choice, etc)
-function candidateSelection(candidate){
-   
+function candidateSelection(candidate) {
+
     orderedCandidate.push(candidate)
     //console.log(orderedCandidate);
 
@@ -14,25 +14,26 @@ function candidateSelection(candidate){
     /*if(orderedCandidate.length > 5){
         orderedCandidate.length = Math.min(orderedCandidate.length, 5);
     }*/
-    
+
     //make a list w/o any duplicates
     listCandidates = [...new Set(orderedCandidate)];
-    
+
     //put name in the ballot list
-    for(let i=0; i<listCandidates.length; i++)
-    {
-        try{
+    for (let i = 0; i < listCandidates.length; i++) {
+        try {
             //console.log(finalCandidates[i]);
-            document.querySelector(`#choice${i+1}`).innerHTML = `${listCandidates[i]}`;
-            finalCandidates[i] = document.querySelector(`#choice${i+1}`).innerHTML;
-        }catch{
+            document.querySelector(`#choice${i + 1}`).innerHTML = `${listCandidates[i]}`;
+            finalCandidates[i] = document.querySelector(`#choice${i + 1}`).innerHTML;
+
+        } catch {
             console.log("Too many candidates");
         }
-        
+
     }
+    stateChange(listCandidates);
     console.log(finalCandidates);
     return finalCandidates;
-    
+
 }
 
 
@@ -41,37 +42,55 @@ function candidateSelection(candidate){
 //html buttons/ slot fills are based on the array of candidates
 
 //submission confirmation: ballet and each candidate choice
-function ballotSubmission(){
-   
-    if(confirm(`Are you sure you want to submit your ballot?`))
-    {
+function ballotSubmission() {
+
+    if (confirm(`Are you sure you want to submit your ballot?`)) {
         //confirm
         console.log("Confirmed");
         console.log(finalCandidates);
         return;
-    }else
-    {
+    } else {
         //cancel
         console.log("Cancelled");
     }
-    
+
 }
 
-function deleteButton(choice)
-{
-    
-    for(let i =0; i<finalCandidates.length; i++)
-    {
-        if(finalCandidates[i] == document.querySelector(`#${choice}`).innerHTML)
-        {
+function deleteButton(choice) {
+
+    for (let i = 0; i < finalCandidates.length; i++) {
+        if (finalCandidates[i] == document.querySelector(`#${choice}`).innerHTML) {
             //empty the spot in array
             finalCandidates[i] = null;
-            finalCandidates.splice(i, i+1);
+            finalCandidates.splice(i, i + 1);
         }
     }
     console.log(finalCandidates);
     return finalCandidates;
-    
+
+}
+
+
+function stateChange(finalCandidates) {
+
+    if (finalCandidates[0] == undefined) {
+        document.getElementById("stateText").innerHTML = "Choose your <b><u>first choice</u></b> candidate";
+
+    } else if (finalCandidates[1] == undefined) {
+        document.getElementById("stateText").innerHTML = "Choose your <b><u>second choice</u></b> candidate";
+
+    } else if (finalCandidates[2] == undefined) {
+        document.getElementById("stateText").innerHTML = "Choose your <b><u>third choice</u></b> candidate";
+
+    } else if (finalCandidates[3] == undefined) {
+        document.getElementById("stateText").innerHTML = "Choose your <b><u>fourth choice</u></b> candidate";
+
+    } else if (finalCandidates[4] == undefined) {
+        document.getElementById("stateText").innerHTML = "Choose your <b><u>fifth choice</u></b> candidate";
+
+    } else if (finalCandidates[5] == undefined) {
+        document.getElementById("stateText").innerHTML = "<b><u>Edit</u></b> your choices or <b><u>Submit</u></b> your ballot";
+    }
 }
 
 //keyboard for write-in? : keyboard button that shows keyboard image
